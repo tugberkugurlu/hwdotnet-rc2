31588b3f901363bde12f23b563fed01f3eabc4ef
Tugberk Ugurlu <tugberk@outlook.com>
2016-05-26 06:20:54 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.DependencyModel;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            var runtimeId = RuntimeEnvironment.GetRuntimeIdentifier();
+            var assemblies = DependencyContext.Default.GetRuntimeAssemblyNames(runtimeId);
+            
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine(assembly.FullName);
+            }
+        }
+    }
+}
