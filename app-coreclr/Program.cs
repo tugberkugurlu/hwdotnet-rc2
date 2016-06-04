8c0cda04e22cb800cd5d4f87d32ab8aedba2bd15
Tugberk Ugurlu <tugberk@outlook.com>
2016-06-04 18:18:37 +0100

    config bind sample

---
+++ app-coreclr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApplication
+{
+    public class PrimaryDataStorageSettings 
+    {
+        public string MongoDbConnectionString { get; set; }
+        public string MongoDbDatabase { get; set; }
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = Path.Combine(ApplicationEnvironment.ApplicationBasePath, "..", "..", "..");
+            var fullPath = Path.GetFullPath(path);
+            
+            Console.WriteLine(path);
+            Console.WriteLine(fullPath);
+            
+            var config = new ConfigurationBuilder()
+                .SetBasePath(fullPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables("CoreClrSample_")
+                .Build();
+            
+            var mongoSettings = new PrimaryDataStorageSettings();
+            ConfigurationBinder.Bind(config.GetSection("PrimaryDataStorage"), mongoSettings);
+            
+            Console.WriteLine(mongoSettings.MongoDbConnectionString);
+            Console.WriteLine(mongoSettings.MongoDbDatabase);
+        }
+    }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.DotNet.InternalAbstractions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyModel;
 using SharedLib;
 
 namespace ConsoleApplication
 {
+    public class Foo 
+    {
+        public string Bar { get; set; }
+    }
+    
     public class Program
     {
         public static void Main(string[] args)
         {   
             var runtimeId = RuntimeEnvironment.GetRuntimeIdentifier();
             var assemblies = DependencyContext.Default.GetRuntimeAssemblyNames(runtimeId);
+            
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string> 
+                {
+                    { "Foo__Bar", "foobar"}
+                })
+                .Build();
+            
+            var foo = config.GetValue(typeof(Foo), "Foo");
             
             Speaker.Speak();
             
